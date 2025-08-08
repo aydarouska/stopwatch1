adf3bfb7f6c3a4bb25aa6d93ac69505f63ab4700
aydarousk <aydarous.shehata@gmail.com>
2025-08-08 14:39:42 +0300

    First commit

---
+++ text.js
@@ -0,0 +1,43 @@
+
+let startTime, updatedTime, difference, tInterval;
+let running = false;
+
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime() - (difference || 0);
+        tInterval = setInterval(updateTime, 1000);
+        running = true;
+    }
+}
+
+function stopTimer() {
+    clearInterval(tInterval);
+    running = false;
+}
+
+function resetTimer() {
+    clearInterval(tInterval);
+    running = false;
+    difference = 0;
+    document.getElementById('display').textContent = "00:00:00";
+}
+
+function updateTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    const formattedTime = 
+        String(hours).padStart(2, '0') + ":" +
+        String(minutes).padStart(2, '0') + ":" +
+        String(seconds).padStart(2, '0');
+
+    document.getElementById('display').textContent = formattedTime;
+}
+
+document.getElementById('start').onclick = startTimer;
+document.getElementById('stop').onclick = stopTimer;
+document.getElementById('reset').onclick = resetTimer;
